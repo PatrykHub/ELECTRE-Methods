88eca553f89080319ea4afbadf620840d5ee47a0
a-pralat <anna@pralat.pl>
2022-10-29 21:35:18 +0200

    Add final ranking

---
+++ mcda/plot/plot.py
@@ -0,0 +1,65 @@
+from typing import Union
+
+import graphviz
+import networkx as nx
+import pandas as pd
+
+
+def outranking_to_graph(
+    outranking_matrix: pd.DataFrame,
+    transitive_reduction: bool = True,
+) -> Union[nx.DiGraph, str]:
+    """Constructs graph from outranking matrix.
+
+    :param outranking_matrix: given outranking matrix
+    :param transitive_reduction: enable transitive reduction, defaults to True
+
+    :return: Graph created from outranking matrix if possible,
+    otherwise returns string as a warning.
+    """
+    graph = nx.DiGraph()
+    for alt_name_a in outranking_matrix.index:
+        for alt_name_b in outranking_matrix.index:
+            if (
+                outranking_matrix.loc[alt_name_a][alt_name_b] == 1
+                and alt_name_a != alt_name_b
+            ):
+                graph.add_edge(alt_name_a, alt_name_b)
+
+    try:
+        return nx.transitive_reduction(graph) if transitive_reduction else graph
+    except nx.NetworkXError:
+        return "Directed Acyclic Graph required for transitive_reduction"
+
+
+def _networkx_graph_to_graphviz(graph: nx.DiGraph) -> graphviz.Digraph:
+    """Transforms NetworkX DiGraph to graphviz Digraph
+
+    :param graph: NetworkX DiGraph
+
+    :return: Graphviz Digraph
+    """
+    new_graph = graphviz.Digraph("graph", strict=True)
+    new_graph.attr("node", shape="box")
+    for alt_name_a, alt_name_b in graph.edges:
+        new_graph.edge(alt_name_a, alt_name_b)
+    return new_graph
+
+
+def plot_outranking(
+    outranking_matrix: pd.DataFrame, transitive_reduction: bool = True
+) -> Union[graphviz.Digraph, str]:
+    """Creates graph plot from outranking matrix
+
+    :param outranking_matrix: given outranking matrix
+    :param transitive_reduction: enable transitive reduction, defaults to True
+
+    :return: Graph created from outranking matrix if possible,
+    otherwise returns string as a warning.
+    """
+    graph = outranking_to_graph(outranking_matrix, transitive_reduction)
+
+    if isinstance(graph, str):
+        return "Directed Acyclic Graph required for transitive_reduction"
+
+    return _networkx_graph_to_graphviz(graph)
--- tests/test_distillation.py
+++ tests/test_distillation.py
@@ -4,11 +4,17 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from mcda.electre.outranking import (alternative_qualities,
-                                     crisp_outranking_relation_distillation,
-                                     distillation,
-                                     get_maximal_credibility_index,
-                                     get_minimal_credibility_index)
+from mcda.electre.outranking import (
+    _get_maximal_credibility_index,
+    _get_minimal_credibility_index,
+    alternative_qualities,
+    crisp_outranking_relation_distillation,
+    distillation,
+    final_ranking_matrix,
+    median_order,
+    order_to_outranking_matrix,
+    ranks,
+)
 
 
 @pytest.fixture
@@ -30,11 +36,11 @@ def credibility_matrix() -> pd.DataFrame:
 
 
 def test_get_maximal_credibility_index(credibility_matrix) -> None:
-    assert get_maximal_credibility_index(credibility_matrix) == 1.0
+    assert _get_maximal_credibility_index(credibility_matrix) == 1.0
 
 
 def test_get_minimal_credibility_index(credibility_matrix) -> None:
-    assert get_minimal_credibility_index(credibility_matrix, 1.0) == 0.8
+    assert _get_minimal_credibility_index(credibility_matrix, 1.0) == 0.8
 
 
 @pytest.mark.parametrize(
@@ -76,9 +82,97 @@ def test_distillation(credibility_matrix) -> None:
     downward = distillation(credibility_matrix)
 
     for i in range(len(downward)):
-        assert downward[i] == expected_downward[i]
+        assert downward[i + 1] == expected_downward[i]
 
     upward = distillation(credibility_matrix, upward_order=True)
 
     for i in range(len(upward)):
-        assert upward[i] == expected_upward[i]
+        assert upward[i + 1] == expected_upward[i]
+
+
+@pytest.fixture
+def downward_order_matrix() -> pd.DataFrame:
+    alt_names_descending: List[str] = ["GER", "ITA", "BEL", "AUT", "FRA"]
+    return pd.DataFrame(
+        [
+            [1, 1, 1, 1, 1],
+            [0, 1, 1, 1, 1],
+            [0, 0, 1, 1, 1],
+            [0, 0, 1, 1, 1],
+            [0, 0, 1, 1, 1],
+        ],
+        index=alt_names_descending,
+        columns=alt_names_descending,
+    )
+
+
+@pytest.fixture
+def upward_order_matrix() -> pd.DataFrame:
+    alt_names_ascending: List[str] = ["FRA", "GER", "ITA", "BEL", "AUT"]
+    return pd.DataFrame(
+        [
+            [1, 1, 1, 1, 1],
+            [0, 1, 1, 1, 1],
+            [0, 0, 1, 1, 1],
+            [0, 0, 0, 1, 1],
+            [0, 0, 0, 0, 1],
+        ],
+        index=alt_names_ascending,
+        columns=alt_names_ascending,
+    )
+
+
+@pytest.fixture
+def downward_order() -> pd.Series:
+    return pd.Series([["GER"], ["ITA"], ["BEL", "AUT", "FRA"]])
+
+
+@pytest.fixture
+def upward_order() -> pd.Series:
+    return pd.Series([["FRA"], ["GER"], ["ITA"], ["BEL"], ["AUT"]])
+
+
+def test_order_to_outranking_matrix(
+    downward_order, downward_order_matrix, upward_order, upward_order_matrix
+) -> None:
+    assert order_to_outranking_matrix(downward_order).equals(downward_order_matrix)
+    assert order_to_outranking_matrix(upward_order).equals(upward_order_matrix)
+
+
+@pytest.fixture
+def final_ranking() -> pd.DataFrame:
+    alt_names: List[str] = ["AUT", "BEL", "FRA", "GER", "ITA"]
+    return pd.DataFrame(
+        [
+            [1, 0, 0, 0, 0],
+            [1, 1, 0, 0, 0],
+            [1, 1, 1, 0, 0],
+            [1, 1, 0, 1, 1],
+            [1, 1, 0, 0, 1],
+        ],
+        index=alt_names,
+        columns=alt_names,
+    )
+
+
+def test_final_ranking_matrix(
+    downward_order_matrix, upward_order_matrix, final_ranking
+) -> None:
+    assert final_ranking_matrix(downward_order_matrix, upward_order_matrix).equals(
+        final_ranking
+    )
+
+
+@pytest.fixture
+def ranks_ranking() -> pd.Series:
+    return pd.Series([["FRA", "GER"], ["ITA"], ["BEL"], ["AUT"]], index=[1, 2, 3, 4])
+
+
+def test_ranks(final_ranking, ranks_ranking) -> None:
+    assert ranks(final_ranking).equals(ranks_ranking)
+
+
+def test_median_order(ranks_ranking, downward_order, upward_order) -> None:
+    assert median_order(ranks_ranking, downward_order, upward_order).equals(
+        pd.Series([["GER"], ["FRA"], ["ITA"], ["BEL"], ["AUT"]], index=[1, 2, 3, 4, 5])
+    )
